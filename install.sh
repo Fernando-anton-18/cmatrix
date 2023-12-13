c622181e013a2c030b66c846d81d50d0c72965f9
Fernando Antón <131510571+Fernando-anton-18@users.noreply.github.com>
2023-12-13 08:52:23 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+pkg install cmatrix
+echo " "
+echo " "
+magenta="\033[38;5;213m"
+echo  -e "$magenta"
+echo "escriba './efecto.sh'. y para la próxima solo entre al directorio y ponga './efecto.sh' o 'bash efecto.sh'"
